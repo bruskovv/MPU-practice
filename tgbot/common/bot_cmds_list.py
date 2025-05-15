3a27b581c6e8f9d7d03adf2372ba50defaa9f3c2
bruskovv <bruskovav06@gmail.com>
2025-05-15 21:37:37 +0300

    Add files via upload

--- tgbot/common/bot_cmds_list.py
+++ tgbot/common/bot_cmds_list.py
@@ -1,1 +1,8 @@
+from aiogram.types import BotCommand
 
+private = [
+    BotCommand(command= 'members', description='Участники'),
+    BotCommand(command= 'about', description = 'О проекте'),
+    BotCommand(command= 'about_partners', description= 'О партнерах'),
+    BotCommand(command= 'project_status', description = 'Ход проекта'),
+]
